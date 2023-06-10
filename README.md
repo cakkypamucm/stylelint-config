c23e981e1e0833fd979b8626a756b374d6430add
cakkypamucm <119052172+cakkypamucm@users.noreply.github.com>
2023-06-10 11:33:15 +0300

    git init

---
+++ README.md
@@ -0,0 +1,7 @@
+This is `stylelint` config (`scss`, `standard-scss`, `idiomatic-order`, `prettier`)
+
+### Install
+
+```shell
+npm i --save-dev github:cakkypamucm/stylelint-config
+```
---
+++ index.js
@@ -0,0 +1,35 @@
+module.exports = {
+    plugins: ["stylelint-no-unsupported-browser-features"],
+    extends: [
+        "stylelint-config-standard-scss",
+        "stylelint-config-idiomatic-order",
+        "stylelint-config-prettier-scss"
+    ],
+    rules: {
+        "color-hex-length": "long",
+        "declaration-block-no-duplicate-properties": [
+            true,
+            {
+                ignore: ["consecutive-duplicates-with-different-values"]
+            }
+        ],
+        "font-family-name-quotes": "always-where-recommended",
+        "import-notation": "string",
+
+        "plugin/no-unsupported-browser-features": [
+            true,
+            {
+                ignore: ["css-logical-props", "css-nesting", "css-overflow", "viewport-unit-variants"],
+                ignorePartialSupport: false,
+                severity: "warning"
+            }
+        ],
+
+        "selector-pseudo-class-no-unknown": [
+            true,
+            {
+                ignorePseudoClasses: ["export"]
+            }
+        ]
+    }
+};
